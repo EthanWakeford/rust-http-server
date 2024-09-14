7b6871811c3f3f6a896dffb404091d7ad10a794e
EthanWakeford <ethanwakeford@gmail.com>
2024-09-14 13:48:17 -0500

    multithreading working

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Rust HTTP Server
 
 Simple Multithreaded HTTP server written in rust built reading the book "The Rust Programming Language"
+w
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,28 @@
-use std::thread;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
 
+type JobReceiver = Arc<Mutex<mpsc::Receiver<Job>>>;
 pub struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: thread::JoinHandle<JobReceiver>,
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, receiver: JobReceiver) -> Worker {
+        // let thread = thread::spawn(|| {receiver});
+
+        let thread = thread::spawn(move || loop {
+            // Call to recv blocks until job is passed
+            // lock ensures only one worker is waiting for a job at a given moment
+            // mutex guard is implicitly dropped after at the end of this statement as it is not the final value
+            let job = receiver.lock().expect("Mutex Poisoned").recv().unwrap();
+
+            println!("Worker {id} got a job; executing.");
+
+            job();
+        });
 
         Worker { id, thread }
     }
@@ -15,7 +30,10 @@ impl Worker {
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
     /// Create a ThreadPool
@@ -28,18 +46,25 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id))
+            workers.push(Worker::new(id, Arc::clone(&receiver)))
         }
 
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+
+        self.sender.send(job).unwrap();
     }
 }
